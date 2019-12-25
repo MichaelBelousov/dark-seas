03dab72796254dac0007296639b28d428bacb344
Michael Belousov <michael.belousov98@gmail.com>
2019-12-25 14:47:09 -0500

    removed old stuff, tweaked shader params

--- public/assets/water/frag.glsl
+++ public/assets/water/frag.glsl
@@ -26,10 +26,12 @@ void main( void ) {
   vec2 noiseUvs = noiseScale * (noiseMotion + vUv);
   vec2 noise = texture2D(texture1, noiseUvs).xy;
   vec2 noiseFactor = vec2(0.3, 0.3);
-  vec2 skewedNoise = noiseFactor * noise;
+  vec2 combinedNoise = noiseFactor * noise;
   
   // foreground caustics
-  vec2 causticsUvs1 = vUv + skewedNoise;
+  vec2 causticsMotion = 1.0 * vec2(0.05 * time, -0.05 * time);
+  vec2 baseCausticsUvs = causticsMotion + vUv * vec2(2.0, 2.0);
+  vec2 causticsUvs1 = baseCausticsUvs + combinedNoise;
   float caustics1 = texture2D(texture2, causticsUvs1).x;
 
   // background caustics
@@ -37,34 +39,6 @@ void main( void ) {
   float caustics2 = texture2D(texture2, causticsUvs2).x;
 
   // mix layers
-  float caustics = addBlacks(caustics1, caustics2, 0.02, 0.006);
+  float caustics = addBlacks(caustics1, caustics2, 0.03, 0.02);
   gl_FragColor = fromBw(caustics);
-
-  //small noise
-/*
-  vec2 noiseScale = 3.0 * vec2(1.0, 1.0);
-  vec2 noiseMove = vec2(time, time) * 0.001;
-  vec2 noiseUvs = noiseScale * (noiseMove + vUv);
-  vec4 noise = 1.0 * texture2D( texture1, noiseUvs);
-  float noisePinch = 0.01;
-  vec2 noiseFactor = vec2(0.1, 0.1);
-  vec2 pinchedNoise = clamp((noise-noisePinch) * (1.0/(1.0-noisePinch)), 0.0, 1.0).xy;
-  vec2 finalNoise = pinchedNoise * noiseFactor;
-
-  vec2 colorScale = vec2(4.0, 4.0);
-  vec2 colorMove = vec2(time, -time) * 0.010;
-  vec2 colorUvs = colorScale * (colorMove + finalNoise + vUv);
-  vec4 color = texture2D( texture2, colorUvs );
-*/
-  //gl_FragColor = color;
-  //gl_FragColor = color;
-
-  /*
-  gl_FragColor = temp;
-  float depth = gl_FragCoord.z / gl_FragCoord.w;
-  const float LOG2 = 1.442695;
-  float fogFactor = exp2( - fogDensity * fogDensity * depth * depth * LOG2 );
-  fogFactor = 1.0 - clamp( fogFactor, 0.0, 1.0 );
-  gl_FragColor = mix( gl_FragColor, vec4( fogColor, gl_FragColor.w ), fogFactor );
-  */
 }
